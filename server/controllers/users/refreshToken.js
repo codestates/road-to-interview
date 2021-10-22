cefae214e597991393fe486c49b5c97fe702b114
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2021-10-22 17:30:19 +0900

    modify : 로그아웃 수정

--- server/controllers/users/refreshToken.js
+++ server/controllers/users/refreshToken.js
@@ -8,47 +8,48 @@ const {
 const { users } = require("../../models");
 
 module.exports = (req, res) => {
-  const refreshToken = req.cookies.refreshToken;
   const accessTokenData = isAuthorized(req);
   if (!accessTokenData) {
-    res
-      .status(401)
-      .send({ message: "토큰 재발급 : 로그인이 만료되었습니다.(accessToken)" });
-    return;
-  }
-  if (!refreshToken) {
-    res
-      .status(401)
-      .send({
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      res.status(401).send({
         message: "토큰 재발급 : 로그인이 만료되었습니다.(refreshToken)",
       });
-    return;
-  }
+      return;
+    }
+    const refreshTokenData = checkRefeshToken(refreshToken);
+    if (!refreshTokenData) {
+      res
+        .status(404)
+        .send({ message: "토큰 재발급 : 일치하는 유저 정보가 없습니다." });
+      return;
+    }
 
-  const refreshTokenData = checkRefeshToken(refreshToken);
-  if (!refreshTokenData) {
-    res
-      .status(404)
-      .send({ message: "토큰 재발급 : 일치하는 유저 정보가 없습니다." });
-    return;
-  }
-
-  const { id } = refreshTokenData;
-  users
-    .findOne({ where: { id } })
-    .then((data) => {
-      if (!data) {
-        res
-          .status(404)
-          .send({ message: "토큰 재발급 : 일치하는 유저 정보가 없습니다." });
-        return;
-      }
-      const { nickname, email, id } = data.dataValues;
-      const newAccessToken = generateAccessToken({ nickname, email, id });
-      resendAccessToken(res, newAccessToken, { nickname, email, id });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: "토큰 재발급 : Server Error" }); // Server error
+    const { id } = refreshTokenData;
+    users
+      .findOne({ where: { id } })
+      .then((data) => {
+        if (!data) {
+          res
+            .status(404)
+            .send({ message: "토큰 재발급 : 일치하는 유저 정보가 없습니다." });
+          return;
+        }
+        const { nickname, email, id } = data.dataValues;
+        const newAccessToken = generateAccessToken({ nickname, email, id });
+        resendAccessToken(res, newAccessToken, { nickname, email, id });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send({ message: "토큰 재발급 : Server Error" }); // Server error
+      });
+  } else {
+    console.log(accessTokenData);
+    const { nickname, email, id } = accessTokenData;
+    resendAccessToken(res, req.headers["authorization"], {
+      nickname,
+      email,
+      id,
     });
+  }
 };
