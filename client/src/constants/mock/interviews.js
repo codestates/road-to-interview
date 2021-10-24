96df7ff3305e76f49986d8888cb578a6431ca1a3
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-24 13:25:18 +0900

    chore: mockup data 추가

---
+++ client/src/constants/mock/interviews.js
@@ -0,0 +1,64 @@
+const interviews = [
+  {
+    id: 1,
+    title: '프론트엔드 면접 문제은행 시리즈1',
+    description: '프론트엔드 일반 문제를 풀어봅시다~',
+    categorys: [
+      {
+        id: 1,
+        name: 'frontend',
+      },
+      {
+        id: 2,
+        name: 'general',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: '프론트엔드 면접 문제은행 시리즈2',
+    description: 'HTML에 대해서 문제를 풀어봅시다.',
+    categorys: [
+      {
+        id: 1,
+        name: 'frontend',
+      },
+      {
+        id: 2,
+        name: 'html',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: '프론트엔드 면접 문제은행 시리즈3',
+    description: 'CSS에 대해서 얼마나 알고 계신가요? 확인해볼까요?',
+    categorys: [
+      {
+        id: 1,
+        name: 'frontend',
+      },
+      {
+        id: 2,
+        name: 'css',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: '프론트엔드 면접 문제은행 시리즈4',
+    description: 'Javascript에 대해서 얼마나 알고 계신가요? 확인해볼까요?',
+    categorys: [
+      {
+        id: 1,
+        name: 'frontend',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+      },
+    ],
+  },
+];
+
+export default interviews;
