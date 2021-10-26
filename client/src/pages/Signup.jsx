e0545ccf1dbf9e55b27a4837ed305d96a0bd6f0b
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-10-27 01:33:58 +0900

    New: 회원가입시 입력창  유효성 검사 - react-hook-form 사용

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 
 import { signup } from '@/store/reducers/users';
 import styled from '@emotion/styled';
@@ -9,40 +10,76 @@ import Label from '@/components/elements/Label';
 import Button from '@/components/elements/Button';
 
 export default function Signup() {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [password, setPassword] = useState('');
+  // * react-hook-form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
 
-  const onSubmit = e => {
-    e.preventDefault();
+  const password = useRef();
+  password.current = watch('password');
 
-    const data = {
-      email,
-      nickname,
-      password,
-    };
+  const onSubmit = data => {
+    console.log(data);
 
-    dispatch(signup(data));
+    // dispatch(signup(data));
   };
 
   return (
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <Field>
         <Label>이메일</Label>
-        <Input placeholder="이메일을 입력하세요." />
+        <Input
+          placeholder="이메일을 입력하세요."
+          {...register('email', {
+            required: { value: true, message: '필수로 입력해야 합니다.' },
+            pattern: { value: /^\S+@\S+$/i, message: '이메일 형식에 맞게 작성해야 합니다.' },
+          })}
+        />
+        {errors?.email && <p>{errors.email.message}</p>}
       </Field>
       <Field>
         <Label>닉네임</Label>
-        <Input placeholder="닉네임을 입력하세요." />
+        <Input
+          placeholder="닉네임을 입력하세요."
+          {...register('nickname', {
+            required: { value: true, message: '필수로 입력해야 합니다.' },
+            minLength: { value: 4, message: '닉네임은 4자 이상이여야 합니다.' },
+            maxLength: { value: 12, message: '닉네임은 12자 이하여야 합니다.' },
+          })}
+        />
+        {errors?.nickname && <p>{errors.nickname.message}</p>}
       </Field>
       <Field>
         <Label>비밀번호</Label>
-        <Input placeholder="비밀번호를 입력하세요." type="password" />
+        <Input
+          ref={password}
+          placeholder="비밀번호를 입력하세요."
+          type="password"
+          {...register('password', {
+            required: { value: true, message: '필수로 입력해야 합니다.' },
+            minLength: { value: 8, message: '비밀번호는 8자 이상이여야 합니다.' },
+          })}
+        />
+        {errors?.password && <p>{errors.password.message}</p>}
       </Field>
       <Field>
         <Label>비밀번호 확인</Label>
-        <Input placeholder="비밀번호를 한번 더 입력하세요." type="password" />
+        <Input
+          placeholder="비밀번호를 한번 더 입력하세요."
+          type="password"
+          {...register('passwordConfirm', {
+            required: true,
+            validate: value => value === password.current,
+          })}
+        />
+        {errors?.passwordConfirm && errors?.passwordConfirm?.type === 'required' && <p>필수로 입력해야 합니다.</p>}
+        {errors?.passwordConfirm && errors?.passwordConfirm?.type === 'validate' && (
+          <p>입력하신 비밀번호와 일치하지 않습니다.</p>
+        )}
       </Field>
       <Button primary lg>
         회원가입
