fd5045299f5d5b9b7fb0c81ff4eca913ceff2ca9
intae kim <intaemon@intaeui-MacBookAir.local>
2021-10-27 16:17:56 +0900

    style: InterviewList 페이지 디자인

--- client/src/pages/InterviewList.jsx
+++ client/src/pages/InterviewList.jsx
@@ -20,117 +20,80 @@ export default function InterviewList() {
 
   return (
     <div>
+      <Tabs
+        currentTab="1"
+        css={css`
+          margin-bottom: 1.5rem;
+        `}
+      >
+        <Tabs.Tab id="1">모든 게시물</Tabs.Tab>
+        <Tabs.Tab id="2">프론트엔드</Tabs.Tab>
+        <Tabs.Tab id="3">백엔드</Tabs.Tab>
+      </Tabs>
       <div>
-        <Table
-          css={css`
-            margin-bottom: 1rem;
-          `}
-        >
-          <Table.Header>
-            <h3
-              css={theme => css`
-                ${theme.typography.subtitle[4]}
-              `}
-            >
-              모든 기술면접 정리(Javascript)
-            </h3>
-          </Table.Header>
-          <Table.Body>
-            <p
-              css={theme => css`
-                ${theme.typography.body[2]}
-                margin-bottom: 0.5em;
-              `}
-            >
-              자바스크립트 기술면접을 대비합니다.
-            </p>
-          </Table.Body>
-          <Table.FooterTop>
-            <UserInfo nickname="Chung Seong Jun" />
-          </Table.FooterTop>
-          <Table.FooterStart
+        {INTERVIEWS.map(interview => (
+          <Table
+            key={interview.id}
             css={css`
-              display: flex;
-              & > * {
-                margin-right: 0.5em;
-              }
+              margin-bottom: 2.5rem;
             `}
           >
-            <Tag>기술면접대비</Tag>
-            <Tag>React</Tag>
-          </Table.FooterStart>
-          <Table.FooterEnd>
-            <Button secondary sm onClick={onOpen}>
-              도전하기
-            </Button>
-          </Table.FooterEnd>
-        </Table>
-        <Table>
-          <Table.Header>
-            <h3
-              css={theme => css`
-                ${theme.typography.subtitle[4]}
+            <Table.Header>
+              <h3
+                css={theme =>
+                  css`
+                    ${theme.typography.subtitle[4]}
+                  `
+                }
+              >
+                {interview.title}
+              </h3>
+            </Table.Header>
+            <Table.Body>
+              <p
+                css={theme =>
+                  css`
+                    ${theme.typography.body[2]};
+                    color: ${theme.colors.text.secondary};
+                  `
+                }
+              >
+                {interview.description}
+              </p>
+            </Table.Body>
+            <Table.FooterTop>
+              <UserInfo nickname={interview.author.nickname} />
+            </Table.FooterTop>
+            <Table.FooterStart
+              css={css`
+                display: flex;
+                align-items: flex-end;
+                flex-wrap: wrap;
+                & > * {
+                  margin-top: 0.5em;
+                }
+                & > *:not(:last-child) {
+                  margin-right: 0.5em;
+                }
               `}
             >
-              리덕스가 무엇인가요?
-            </h3>
-          </Table.Header>
-          <Table.Body>
-            <p
-              css={theme => css`
-                margin-bottom: 0.5em;
-                ${theme.typography.body[2]}
-              `}
-            >
-              나의답변:
-            </p>
-            <p
-              css={theme => css`
-                margin-bottom: 0.5em;
-                ${theme.typography.body[2]}
-              `}
-            >
-              리덕스는 리액트의 전역상태를 관리하는 라이브러리 입니다. 다른 라이브러리와 다른 특징은 미들웨어를 사용할
-              수 있다는 점입니다. 그리고...
-            </p>
-          </Table.Body>
-          <Table.FooterTop>
-            <UserInfo nickname="Chung Seong Jun" />
-          </Table.FooterTop>
-          <Table.FooterStart>
-            <Button secondary sm onClick={onOpen}>
-              베스트답변 보러가기
-            </Button>
-          </Table.FooterStart>
-        </Table>
-        <Table>
-          <Table.Header>
-            <UserInfo nickname="Chung Seong Jun" answer />
-          </Table.Header>
-          <Table.Body>
-            <p
-              css={theme => css`
-                margin-bottom: 0.5em;
-                ${theme.typography.body[2]}
-              `}
-            >
-              이것은 해당 질문의 베스트 답변입니다. 당신도 어서 베스트 답변을 위해서 공부하고 도전하세요. 이것은 해당
-              질문의 베스트 답변입니다. 당신도 어서 베스트 답변을 위해서 공부하고 도전하세요. 이것은 해당 질문의 베스트
-              질문의 베스트 답변입니다. 당신도...
-            </p>
-          </Table.Body>
-          <Table.FooterTop>
-            <Button secondary sm onClick={onOpen}>
-              좋아요
-            </Button>
-          </Table.FooterTop>
-        </Table>
+              {interview.categorys.map(category => (
+                <Tag key={category.id}>{category.name}</Tag>
+              ))}
+            </Table.FooterStart>
+            <Table.FooterEnd>
+              <Button sm secondary onClick={onOpen}>
+                도전하기
+              </Button>
+            </Table.FooterEnd>
+          </Table>
+        ))}
       </div>
       <Portal selector="#modal">
         <Modal open={open} onClose={onClose}>
           <div
             css={theme => css`
-              width: 250px;
+              width: 350px;
               height: 500px;
               display: flex;
               justify-content: center;
@@ -139,7 +102,15 @@ export default function InterviewList() {
               color: ${theme.colors.text.primary};
             `}
           >
-            <h1>안내사항</h1>
+            <h1
+              css={theme =>
+                css`
+                  ${theme.typography.subtitle[4]}
+                `
+              }
+            >
+              안내사항
+            </h1>
           </div>
         </Modal>
       </Portal>
