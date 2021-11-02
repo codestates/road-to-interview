2639444215847b682d2ee1c67f413070efaa4316
intae kim <intaemon@intaeui-MacBookAir.local>
2021-11-02 14:37:57 +0900

    new: Landing.jsx react-slick

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+import Slider from 'react-slick';
 
 import { spacing, palette, fontSizes } from '@/styles';
 import Button from '@/components/elements/Button';
@@ -10,177 +12,151 @@ import { ReactComponent as Test } from 'assets/test-ill.svg';
 import { ReactComponent as Feedback } from 'assets/checklist-ill.svg';
 import { ReactComponent as Share } from 'assets/share-ill.svg';
 import media from '@/utils/media';
+import Flex from '@/components/layouts/Flex';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 500,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  appendDots: dots => <ul>{dots}</ul>,
+  customPaging: () => (
+    <div className="dots__dot">
+      <span></span>
+    </div>
+  ),
+};
+
+const footerData = [
+  {
+    href: 'https://github.com/scvd03',
+    src: '/images/intae.png',
+    alt: '김인태',
+    name: '김인태',
+    Platform: 'Github',
+  },
+  {
+    href: 'https://github.com/yg-kim-korean',
+    src: '/images/yonggun.png',
+    alt: '김용건',
+    name: '김용건',
+    Platform: 'Github',
+  },
+  {
+    href: 'https://github.com/jvn4dev',
+    src: '/images/sungjun.png',
+    alt: '정성준',
+    name: '정성준',
+    Platform: 'Github',
+  },
+  {
+    href: 'https://github.com/findmytrueself',
+    src: '/images/hun.png',
+    alt: '임훈',
+    name: '임훈',
+    Platform: 'Github',
+  },
+];
 
 export default function Landing() {
   const history = useHistory();
+  const scrollTo = id => {
+    const element = document.getElementById(id);
+    if (!element) return;
+    element.scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
   return (
-    <Layout>
-      <Header>
-        <Box>
-          <Title>
-            Road To <HighLight>Interview</HighLight>
-          </Title>
-          <Caption>Rehearsal for Practicing for a Job Interview</Caption>
-          <SubTitle>Road To Interview는 개발자 기술면접 인터뷰를 연습할 수 있는 웹 서비스입니다.</SubTitle>
-          <Links>
-            <Button round primary onClick={() => history.push('/list')}>
-              둘러보기
-            </Button>
-            <Button text tertiary icon={Chv}>
-              더 알아보기
-            </Button>
-          </Links>
-        </Box>
-        <Box>
-          <Illustration>
-            <Job width="100%" height="100%" />
-          </Illustration>
-        </Box>
-      </Header>
-      <Sections>
-        <Section>
-          <Illustration>
-            <Test width="100%" height="100%" />
-          </Illustration>
-          <SectionTitle>인터뷰 테스트</SectionTitle>
-          <Text>
-            제한시간 내에 준비된 인터뷰 질문에 답하는 테스트를 진행할 수 있습니다. 실제 화상면접처럼, 웹캠을 키고
-            말해보세요!
-          </Text>
-        </Section>
-        <Section>
-          <Illustration>
-            <Feedback width="100%" height="100%" />
-          </Illustration>
-          <SectionTitle>개인 피드백</SectionTitle>
-          <Text>녹화된 테스트 영상과 관리자가 준비한 모범 답변을 보면서 피드백하세요!</Text>
-        </Section>
-        <Section>
-          <Illustration>
-            <Share width="100%" height="100%" />
-          </Illustration>
-          <SectionTitle>공유하기</SectionTitle>
-          <Text>인터뷰 질문 목록과 답변을 다른 사람들과 공유할 수 있습니다!</Text>
-        </Section>
-      </Sections>
-      <Section
-        css={css`
-          height: 50vh;
-          position: relative;
-          top: ${spacing[6]};
-        `}
-      >
-        <Footer>
-          <div
-            css={css`
-              margin-top: auto;
-              margin-bottom: auto;
-            `}
-          >
-            <div
-              css={css`
-                font-size: ${fontSizes[800]};
-                text-align: center;
-                margin-bottom: ${spacing[3]};
-              `}
-            >
-              TEAM KKAN-BU
-            </div>
-            <div
-              css={css`
-                display: flex;
-                height: 20vh;
-              `}
-            >
-              <a href="https://github.com/scvd03">
-                <TeamProfile>
-                  <TeamImg>
-                    <img
-                      css={css`
-                        width: 100%;
-                        height: 100%;
-                      `}
-                      src="/images/intae.png"
-                      alt="intae"
-                    />
-                  </TeamImg>
-                  <TeamName>김인태</TeamName>
-                  <TeamName>Github</TeamName>
-                </TeamProfile>
-              </a>
-              <a href="https://github.com/yg-kim-korean">
-                <TeamProfile>
-                  <TeamImg>
-                    <img
-                      css={css`
-                        width: 100%;
-                        height: 100%;
-                      `}
-                      src="/images/yonggun.png"
-                      alt="intae"
-                    />
-                  </TeamImg>
-                  <TeamName>김용건</TeamName>
-                  <TeamName>Github</TeamName>
-                </TeamProfile>
-              </a>
-              <a href="https://github.com/jvn4dev">
-                <TeamProfile>
-                  <TeamImg>
-                    <img
-                      css={css`
-                        width: 100%;
-                        height: 100%;
-                      `}
-                      src="/images/sungjun.png"
-                      alt="intae"
-                    />
-                  </TeamImg>
-                  <TeamName>정성준</TeamName>
-                  <TeamName>Github</TeamName>
-                </TeamProfile>
-              </a>
-              <a href="https://github.com/findmytrueself">
-                <TeamProfile>
-                  <TeamImg>
-                    <img
-                      css={css`
-                        width: 100%;
-                        height: 100%;
-                      `}
-                      src="/images/hun.png"
-                      alt="intae"
-                    />
-                  </TeamImg>
-                  <TeamName>임&nbsp;&nbsp;&nbsp;훈</TeamName>
-                  <TeamName>Github</TeamName>
-                </TeamProfile>
-              </a>
-            </div>
-          </div>
-          <div
-            css={css`
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              width: 80vw;
-              /* border: solid; */
-              margin: auto;
-            `}
-          >
-            <a href="https://github.com/codestates/road-to-interview/wiki">
-              <Button secondary lg>
-                Wiki페이지로 가기
+    <>
+      <Layout>
+        <Header>
+          <Box>
+            <Title>
+              Road To <HighLight>Interview</HighLight>
+            </Title>
+            <Caption>Rehearsal for Practicing for a Job Interview</Caption>
+            <SubTitle>Road To Interview는 개발자 기술면접 인터뷰를 연습할 수 있는 웹 서비스입니다.</SubTitle>
+            <Links>
+              <Button round primary onClick={() => history.push('/list')}>
+                둘러보기
               </Button>
+              <Button text tertiary icon={Chv} onClick={() => scrollTo('section')}>
+                더 알아보기
+              </Button>
+            </Links>
+          </Box>
+          <Box>
+            <Illustration>
+              <Job width="100%" height="100%" />
+            </Illustration>
+          </Box>
+        </Header>
+        <Sections id="section">
+          <StyledSlider {...settings}>
+            <Section>
+              <Illustration>
+                <Test width="100%" height="100%" />
+              </Illustration>
+              <SectionTitle>인터뷰 테스트</SectionTitle>
+              <Text>
+                제한시간 내에 준비된 인터뷰 질문에 답하는 테스트를 진행할 수 있습니다. 실제 화상면접처럼, 웹캠을 키고
+                말해보세요!
+              </Text>
+            </Section>
+            <Section>
+              <Illustration>
+                <Feedback width="100%" height="100%" />
+              </Illustration>
+              <SectionTitle>개인 피드백</SectionTitle>
+              <Text>녹화된 테스트 영상과 관리자가 준비한 모범 답변을 보면서 피드백하세요!</Text>
+            </Section>
+            <Section>
+              <Illustration>
+                <Share width="100%" height="100%" />
+              </Illustration>
+              <SectionTitle>공유하기</SectionTitle>
+              <Text>인터뷰 질문 목록과 답변을 다른 사람들과 공유할 수 있습니다!</Text>
+            </Section>
+          </StyledSlider>
+        </Sections>
+      </Layout>
+      <Footer>
+        <h3
+          css={css`
+            font-size: ${fontSizes[800]};
+            text-align: center;
+          `}
+        >
+          TEAM KKAN-BU
+        </h3>
+        <Flex>
+          {footerData.map(({ src, alt, name, href, Platform }) => (
+            <a href={href}>
+              <TeamProfile>
+                <TeamImg src={src} alt={alt} />
+                <TeamName>{name}</TeamName>
+                <TeamName>{Platform}</TeamName>
+              </TeamProfile>
             </a>
-          </div>
-        </Footer>
-      </Section>
-    </Layout>
+          ))}
+        </Flex>
+        <a href="https://github.com/codestates/road-to-interview/wiki">
+          <Button secondary lg>
+            Wiki페이지로 가기
+          </Button>
+        </a>
+      </Footer>
+    </>
   );
 }
 
 const Layout = styled.div`
+  position: relative;
   padding: 0 ${spacing[5]};
 `;
 
@@ -217,6 +193,47 @@ const SubTitle = styled.p`
   font-weight: 400;
   line-height: 1.4em;
 `;
+// * Slider
+const StyledSlider = styled(Slider)`
+  width: 80%;
+  margin: 0 auto;
+
+  .slick-list {
+    overflow: hidden;
+  }
+  .slick-track {
+    display: flex;
+    align-items: center;
+  }
+  .slick-slide {
+  }
+
+  .slick-dots {
+    display: flex;
+    justify-content: center;
+    padding: ${spacing[5]} 0;
+
+    & > *:not(:last-child) {
+      margin-right: 0.7em;
+    }
+
+    .slick-active {
+      & span {
+        background: ${({ theme }) => theme.colors.text.primary};
+      }
+    }
+  }
+
+  .dots__dot {
+    & > span {
+      display: inline-block;
+      width: 0.8em;
+      height: 0.8em;
+      border-radius: 50%;
+      background: ${({ theme }) => theme.colors.text.disable_placeholder};
+    }
+  }
+`;
 
 // * Links
 const Links = styled.div`
@@ -239,20 +256,21 @@ const Links = styled.div`
 const Illustration = styled.div``;
 
 // * Sections
-const Sections = styled.section``;
-const Section = styled.div`
+const Sections = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
   height: 100vh;
+`;
+const Section = styled.section`
+  text-align: center;
+  justify-content: center;
 `;
 const SectionTitle = styled.h3`
   ${({ theme }) => theme.typography.subtitle[2]}
   margin: ${spacing[6]} 0;
 `;
 const Text = styled.p`
-  width: 80%;
   text-align: center;
   word-break: keep-all;
   line-height: 1.5em;
@@ -262,13 +280,14 @@ const Text = styled.p`
 
 // * Footer
 const Footer = styled.div`
-  background: ${palette.light.gray[800]};
+  width: 100vw;
+  padding: ${spacing[7]} 0;
+  margin-bottom: -${spacing[5]};
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  background: ${palette.light.gray[800]};
   color: ${palette.light.gray[300]};
 `;
 
@@ -285,6 +304,6 @@ const TeamProfile = styled.div`
   `)}
 `;
 
-const TeamImg = styled.div`
+const TeamImg = styled.img`
   height: 15vh;
 `;
