28ecb8b75ea988ed88e0f53fc98476c300500ef1
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-11-07 17:44:29 +0900

    Improve: 로그인페이지 디자인 리뉴얼

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import media from '@/utils/media';
 
 import Input from '@/components/elements/Input';
 import Label from '@/components/elements/Label';
@@ -45,88 +46,122 @@ export default function Login() {
   }, [loginError, kakaoLoginError, googleLoginError]);
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Title
-        css={theme => css`
-          ${theme.typography.subtitle[3]}
-          margin-bottom: 1rem;
-        `}
-      >
-        로그인
-      </Title>
-      <Field>
-        <Label>이메일</Label>
-        <Input
-          placeholder="이메일을 입력하세요."
-          {...register('email', {
-            required: { value: true, message: '필수로 입력해야 합니다.' },
-            pattern: { value: /^\S+@\S+$/i, message: '이메일 형식에 맞게 작성해야 합니다.' },
-          })}
-        />
-        {errors?.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
-      </Field>
-      <Field>
-        <Label>패스워드</Label>
-        <Input
-          placeholder="패스워드를 입력하세요."
-          type="password"
-          {...register('password', {
-            required: { value: true, message: '필수로 입력해야 합니다.' },
-            minLength: { value: 4, message: '비밀번호는 4자 이상이여야 합니다.' },
-          })}
-        />
-        {errors?.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
-      </Field>
-      <Button primary lg>
-        로그인하기
-      </Button>
-      <Label
-        css={theme => css`
-          text-align: center;
-          ${theme.typography.caption[2]}
-          opacity: 0.5;
-          margin: 1.25rem 0;
-        `}
-      >
-        또는
-      </Label>
-      <Field>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Title>로그인</Title>
+        <Field>
+          <Field
+            css={css`
+              margin-bottom: 0.75rem;
+            `}
+          >
+            <GoogleSocialLogin />
+          </Field>
+          <KakaoLogin />
+        </Field>
+        <UnderlineBox>
+          <Label
+            css={theme => css`
+              text-align: center;
+              ${theme.typography.caption[2]}
+              background-color: ${theme.colors.background};
+              position: absolute;
+              width: 50px;
+            `}
+          >
+            또는
+          </Label>
+        </UnderlineBox>
+
+        <Field>
+          <Label>이메일</Label>
+          <Input
+            placeholder="이메일을 입력하세요."
+            {...register('email', {
+              required: { value: true, message: '필수로 입력해야 합니다.' },
+              pattern: { value: /^\S+@\S+$/i, message: '이메일 형식에 맞게 작성해야 합니다.' },
+            })}
+          />
+          {errors?.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </Field>
+        <Field>
+          <Label>패스워드</Label>
+          <Input
+            placeholder="패스워드를 입력하세요."
+            type="password"
+            {...register('password', {
+              required: { value: true, message: '필수로 입력해야 합니다.' },
+              minLength: { value: 4, message: '비밀번호는 4자 이상이여야 합니다.' },
+            })}
+          />
+          {errors?.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+        </Field>
+        <Field>
+          <Button
+            primary
+            lg
+            css={css`
+              margin-top: 0.2rem;
+            `}
+          >
+            로그인 하기
+          </Button>
+        </Field>
+
         <Field
           css={css`
-            margin-bottom: 0.75rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
           `}
         >
-          <GoogleSocialLogin />
+          <Label>아직 계정이 없으신가요?</Label>
+          <Label
+            css={theme =>
+              css`
+                color: ${theme.colors.tint.coral[500]};
+                margin-left: 0.25rem;
+                cursor: pointer;
+              `
+            }
+            onClick={() => history.push('/signup')}
+          >
+            계정 만들기
+          </Label>
         </Field>
-        <KakaoLogin />
-      </Field>
-      <Field
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        `}
-      >
-        <Label>아직 계정이 없으신가요?</Label>
-        <Label
-          css={theme =>
-            css`
-              color: ${theme.colors.tint.coral[500]};
-              margin-left: 0.25rem;
-              cursor: pointer;
-            `
-          }
-          onClick={() => history.push('/signup')}
-        >
-          계정 만들기
-        </Label>
-      </Field>
-    </Form>
+      </Form>
+    </Container>
   );
 }
 
-const Form = styled.form``;
-const Field = styled.div`
-  margin-bottom: 1.5rem;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
-const Title = styled.p``;
+const Form = styled.form`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const UnderlineBox = styled.div`
+  width: 150px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.5;
+  height: auto;
+  margin: 1.5rem 0;
+  border-bottom: 1px solid rgba(1, 1, 1, 0.5);
+`;
+const Field = styled.div`
+  margin-bottom: 0.5rem;
+  width: 100%;
+`;
+const Title = styled.p`
+  ${({ theme }) => theme.typography.subtitle[3]}
+  margin-bottom: 1.5rem;
+  text-align: center;
+`;
