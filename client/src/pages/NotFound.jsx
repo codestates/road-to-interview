3b4d013b6cfbb5e711dbcae986aad6c85b190722
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-11-10 01:09:46 +0900

    New: NotFound 페이지 추가

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,12 +1,29 @@
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import styled from '@emotion/styled';
+import { ReactComponent as NotFoundIll } from 'assets/undraw_not_found_-60-pq.svg';
+import Button from '@/components/elements/Button';
 
 export default function NotFound() {
   const history = useHistory();
 
   return (
-    <div>
-      해당 요청을 찾을 수 없습니다~
-      <button onClick={() => history.replace('/')}>홈으로 가기</button>
-    </div>
+    <Layout>
+      <NotFoundIll width="30rem" height="30rem" />
+      <Text>앗! 잘못된 요청입니다.</Text>
+      <Button secondary md onClick={() => history.replace('/')}>
+        홈으로 가기
+      </Button>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.p`
+  ${({ theme }) => theme.typography.subtitle[2]}
+  margin-bottom: 1rem;
+`;
