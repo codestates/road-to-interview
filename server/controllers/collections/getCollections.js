cfeabacc0fa803222eb9db021bc39b5d54b9ec93
yg kim <54833991+yg-kim-korean@users.noreply.github.com>
2021-11-15 17:22:28 +0900

    modify : collections 수정

--- server/controllers/collections/getCollections.js
+++ server/controllers/collections/getCollections.js
@@ -11,8 +11,8 @@ module.exports = (req, res) => {
   const { id } = accessTokenData;
   sequelize
     .query(
-      `select c.id as collections_id ,a.interviews_id, a.title, a.description, a.users_id 
-      from (select i.id as interviews_id, i.title, i.description, i.users_id as users_id from interviews i join users u on i.users_id = u.id) a
+      `select c.id as collections_id ,a.interviews_id, a.title, a.description, a.users_id, a.nickname
+      from (select i.id as interviews_id, i.title, i.description, i.users_id as users_id,u.nickname from interviews i join users u on i.users_id = u.id) a
       join collections c
       on a.interviews_id = c.interviews_id 
       where c.users_id = ` +
